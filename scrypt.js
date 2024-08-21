c1a89a24d91388a7e9a09be0d85e08347da7f01b
Poooseee <agusposeuala@gmail.com>
2024-08-21 00:14:56 -0300

    avance js

--- scrypt.js
+++ scrypt.js
@@ -9,15 +9,16 @@ window.addEventListener('scroll', function() {
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      const target = document.querySelector(this.getAttribute('href'));
-      const navbarHeight = document.querySelector('header').offsetHeight;
-      const offsetTop = target.offsetTop - navbarHeight;
-  
-      window.scrollTo({
-        top: offsetTop,
-        behavior: 'smooth'
-      });
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const navbarHeight = document.querySelector('header').offsetHeight;
+        const offsetTop = target.offsetTop - navbarHeight + 120; // Ajuste adicional de 50px
+
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
     });
-  });
+});
+
